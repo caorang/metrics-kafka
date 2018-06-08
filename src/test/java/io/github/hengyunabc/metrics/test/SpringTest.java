16d9ac90acf7a9257362d76e20197a2c335b1983
rcao1@xiaohongshu.com <>
2018-06-08 09:16:45 +0800

    test

--- src/test/java/io/github/hengyunabc/metrics/test/SpringTest.java
+++ src/test/java/io/github/hengyunabc/metrics/test/SpringTest.java
@@ -33,10 +33,10 @@ public class SpringTest {
 	}
 	
 	public void startKafkaReporter(){
-		String hostName = "192.168.66.30";
-		String topic = "test-kafka-reporter";
+		String hostName = "localhost";
+		String topic = "test";
 		Properties props = new Properties();
-		props.put("metadata.broker.list", "192.168.90.147:9091");
+		props.put("metadata.broker.list", "localhost:9092");
 		props.put("serializer.class", "kafka.serializer.StringEncoder");
 		props.put("partitioner.class", "kafka.producer.DefaultPartitioner");
 		props.put("request.required.acks", "1");
